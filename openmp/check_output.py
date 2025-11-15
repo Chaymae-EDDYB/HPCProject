faf951c6cca32e895ee4a0e97669506bc953771c
chaymae Ed-dyb <chaymaeed571@gmail.com>
2025-11-15 18:18:45 +0000

    check_output.py

--- openmp/check_output.py
+++ openmp/check_output.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Chargement
 X = np.loadtxt("data/data_X.txt")
 y = np.loadtxt("data/data_y.txt", dtype=int)
 W1 = np.loadtxt("output/W1.txt")
@@ -9,7 +8,6 @@ b1 = np.loadtxt("output/b1.txt")
 W2 = np.loadtxt("output/W2.txt")
 b2 = np.loadtxt("output/b2.txt")
 
-# Remettre en forme
 nn_input_dim = 2
 nn_hdim = len(b1)
 nn_output_dim = len(b2)
@@ -19,7 +17,6 @@ W2 = W2.reshape((nn_hdim, nn_output_dim))
 b1 = b1.reshape((1, nn_hdim))
 b2 = b2.reshape((1, nn_output_dim))
 
-# Forward identique au C
 def forward(X):
     a1 = np.tanh(X.dot(W1) + b1)
     z2 = a1.dot(W2) + b2
@@ -27,7 +24,6 @@ def forward(X):
     probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
     return np.argmax(probs, axis=1)
 
-# Grille
 x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
 y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
 xx, yy = np.meshgrid(np.linspace(x_min, x_max, 200),
